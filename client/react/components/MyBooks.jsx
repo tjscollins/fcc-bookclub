6370dfdbd90c292402a8c6b2652bc925968ec35a
Tyler Collins <tjscollins@gmail.com>
2017-04-02 07:35:31 +1000

    Implementing form to add books to personal catalogue

--- client/react/components/MyBooks.jsx
+++ client/react/components/MyBooks.jsx
@@ -6,10 +6,41 @@ import {connect} from 'react-redux';
 import Header from 'Header';
 
 export class MyBooks extends Component {
+  submit = (e) => {
+    e.preventDefault();
+    const {newBook} = this.refs;
+  }
   render() {
     return (
       <div>
         <Header />
+        <br />
+        <div className='container'>
+          <div className='row'>
+            <button className='btn btn-success'>
+              Your trade requests
+            </button>
+            &nbsp;
+            <button className='btn btn-primary'>
+              Trade requests for you
+            </button>
+          </div>
+          <div className='row'>
+            <h1>My Books:</h1>
+            <hr />
+          </div>
+          <div className='row'>
+            <form className='form-inline' onSubmit={this.submit}>
+              <div className='form-group'>
+                <input className='form-control' type='text' ref='newBook' required />
+                &nbsp;
+                <button onClick={this.submit} className='btn btn-primary'>
+                  Add
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
