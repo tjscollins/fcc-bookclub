36253ea6827fe35385346f02c628607ee1de0107
Tyler Collins <tjscollins@gmail.com>
2017-04-01 10:36:13 +1000

    Implement Header component

---
+++ client/react/components/Header.jsx
@@ -0,0 +1,59 @@
+/*----------Modules----------*/
+import React, {Component} from 'react';
+
+/*----------Components----------*/
+
+export class Header extends Component {
+  constructor() {
+    super();
+    this.propTypes = {};
+  }
+  render() {
+    return (
+      <nav className='navbar navbar-default'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <button
+              type='button'
+              className='navbar-toggle collapsed'
+              data-toggle='collapse'
+              data-target='#bs-example-navbar-collapse-1'
+              aria-expanded='false'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+            </button>
+            <a className='navbar-brand' href='#'>bookBazaar</a>
+          </div>
+
+          <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+            <ul className='nav navbar-nav'>
+              <li className='active'>
+                <a href='#'>Home
+                  <span className='sr-only'>(current)</span>
+                </a>
+              </li>
+            </ul>
+
+            <ul className='nav navbar-nav navbar-right'>
+              <li>
+                <a href='#'>Sign up</a>
+              </li>
+              <li>
+                <a href='#'>Login</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+//
+// Header.propTypes = {
+//
+// };
+
+export default Header;
